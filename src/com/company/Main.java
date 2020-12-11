8ae30dde55378858da01b32f1390bc2c65888f80
jdgarciaa11 <74458542+jdgarciaa11@users.noreply.github.com>
2020-12-11 13:43:05 +0100

    Cambios en el main y nuevo comentario

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
-
+/*
+* Nombre escrito
+*/
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("BUENAS MUNDO");
+        System.out.println("Jesus Domingp Garcia Garcia");
     }
 }
